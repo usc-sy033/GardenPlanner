b31de929252190e92e6ebe8ced94f878f6de236a
usc-sy033 <48264586+usc-sy033@users.noreply.github.com>
2019-04-03 07:54:05 +1000

    changed size and background colour

--- src/garden_planner/gui/GuiMain.java
+++ src/garden_planner/gui/GuiMain.java
@@ -5,16 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 
 public class GuiMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
-        Button root = new Button("TODO: design garden planner GUI");
+        Pane root = new VBox();
+        root.setStyle("-fx-background-color: #007700;");
+        root.getChildren().add(new Button("TODO: design garden planner GUI"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
