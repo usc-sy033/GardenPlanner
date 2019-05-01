c7dad53cb4122df7d86820084a70b13ca84b173b
usc-sy033 <48264586+usc-sy033@users.noreply.github.com>
2019-05-01 15:19:55 +1000

    resize circle

--- src/garden_planner/gui/Controller.java
+++ src/garden_planner/gui/Controller.java
@@ -11,9 +11,11 @@ import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
@@ -196,10 +198,17 @@ public class Controller {
                 circle.setOnMouseDragged(ev -> {
 
                     System.out.println("x=" + ev.getX() + " r=" + bed);
-                    circle.setCenterX(ev.getX());
-                    circle.setCenterY(ev.getY());
-                    bed.setLeft(ev.getX() / metre);
-                    bed.setTop(ev.getY() / metre);
+                    if (ev.getX() > circle.getCenterX() + circle.getRadius() / 2 && ev.getY() > circle.getCenterY() + circle.getRadius() / 2) {
+                        circle.setRadius(ev.getX() - circle.getCenterX());
+                        circle.setRadius(ev.getY() - circle.getCenterY());
+                    }
+                    else{
+                        circle.setCenterX(ev.getX());
+                        circle.setCenterY(ev.getY());
+                        bed.setLeft(ev.getX() / metre);
+                        bed.setTop(ev.getY() / metre);
+                    }
+
                 });
             }
 
@@ -214,101 +223,5 @@ public class Controller {
 
         totalCost.setText(String.format("Total garden cost: $ %7.2f", planner.getTotalCost()));
     }
-  /**  public class DragResizer {
 
-        /**
-         * The margin around the control that a user can click in to start resizing
-         * the region.
-         */
-     /**   private static final int RESIZE_MARGIN = 5;
-
-        private final Rectangle region;
-
-        private double y;
-
-        private boolean initMinHeight;
-
-        private boolean dragging;
-
-        private DragResizer(Region aRegion) {
-            region = aRegion;
-        }
-
-        public static void makeResizable(Region region) {
-            final DragResizer resizer = new DragResizer(region);
-
-            region.setOnMousePressed(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    resizer.mousePressed(event);
-                }});
-            region.setOnMouseDragged(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    resizer.mouseDragged(event);
-                }});
-            region.setOnMouseMoved(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    resizer.mouseOver(event);
-                }});
-            region.setOnMouseReleased(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    resizer.mouseReleased(event);
-                }});
-        }
-
-        protected void mouseReleased(MouseEvent event) {
-            dragging = false;
-            region.setCursor(Cursor.DEFAULT);
-        }
-
-        protected void mouseOver(MouseEvent event) {
-            if(isInDraggableZone(event) || dragging) {
-                region.setCursor(Cursor.S_RESIZE);
-            }
-            else {
-                region.setCursor(Cursor.DEFAULT);
-            }
-        }
-
-        protected boolean isInDraggableZone(MouseEvent event) {
-            return event.getY() > (region.getHeight() - RESIZE_MARGIN);
-        }
-
-        protected void mouseDragged(MouseEvent event) {
-            if(!dragging) {
-                return;
-            }
-
-            double mousey = event.getY();
-
-            double newHeight = region.getMinHeight() + (mousey - y);
-
-            region.setMinHeight(newHeight);
-
-            y = mousey;
-        }
-
-        protected void mousePressed(MouseEvent event) {
-
-            // ignore clicks outside of the draggable margin
-            if(!isInDraggableZone(event)) {
-                return;
-            }
-
-            dragging = true;
-
-            // make sure that the minimum height is set to the current height once,
-            // setting a min height that is smaller than the current height will
-            // have no effect
-            if (!initMinHeight) {
-                region.setMinHeight(region.getHeight());
-                initMinHeight = true;
-            }
-
-            y = event.getY();
-        }
-    }*/
 }
